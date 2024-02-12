9fe219bea6b7c0609e1e682aae7c2b250f41a1b6
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2024-02-11 22:02:53 -0500

    dummy data

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import "./Feed.css";
 import Share from '../share/Share';
 import Post from '../post/Post';
+import {Posts} from "dummydata.js";
 
 function Feed() {
   return (
     <div className='feed'>
       <div className="feedWrapper">
           <Share />
+          {Posts.map(p=>{
+            <Post key={p.id} post={p}/>
+          })}
           <Post/>
       </div>
     </div>
